1ec8ffb4c941817ac644d7d95f4c64825a347394
Peter Teuben <teuben@gmail.com>
2020-10-08 14:34:07 -0400

    example for Mel Wright

---
+++ src/scripts/python/cheat1.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+#
+#    example to read a mirid image if you have no other interfaces
+#    needs astropy
+
+import os
+import sys
+import numpy as np
+
+try:
+    from astropy.io import fits
+except:
+    import pyfits as fits
+
+
+def mirread(mfile):
+    cmd = "fits in=%s out=%s/fits op=xyout" % (mfile,mfile)
+    os.system(cmd)
+    hdu = fits.open("%s/fits" % mfile)
+    h = hdu[0].header
+    d = hdu[0].data
+    hdu.close()
+    return(h,d)
+
+
+mfile = sys.argv[1]
+
+
+if not os.path.exists(mfile):
+    print("miriad data %s does not exist" % mfile)
+
+(header,data) = mirread(mfile)
+print(mfile,header["NAXIS1"],header["NAXIS2"],data.mean(), data.std())
